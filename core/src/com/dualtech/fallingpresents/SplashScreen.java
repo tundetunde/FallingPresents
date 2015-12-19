c4d1c59c11574368a546ed4a62e93e7261897855
Jessica <adajess01@gmail.com>
2015-12-19 20:13:58 +0000

    Buttons / Splash BG V5

--- core/src/com/dualtech/fallingpresents/SplashScreen.java
+++ core/src/com/dualtech/fallingpresents/SplashScreen.java
@@ -1,16 +1,34 @@
 package com.dualtech.fallingpresents;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Timer;
 
 /**
  * Created by tunde_000 on 13/12/2015.
  */
 public class SplashScreen extends State{
+    private SpriteBatch batch;
+    OrthographicCamera camera;
+    Texture bgTexture;
+    Sprite bgSprite;
 
     protected SplashScreen(final GameStateManager gcm) {
         super(gcm);
         AssetLoader.load();
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        camera = new OrthographicCamera();
+        batch = new SpriteBatch();
+        bgTexture = AssetLoader.splash;
+        bgSprite = new Sprite(bgTexture);
+        bgSprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(false, FallingPresentsGame.WIDTH / 2, FallingPresentsGame.HEIGHT / 2);
+
         Timer.schedule(new Timer.Task() {
             @Override
             public void run() {
@@ -26,7 +44,12 @@ public class SplashScreen extends State{
     public void update(float dt) {}
 
     @Override
-    public void render(SpriteBatch sb) {}
+    public void render(SpriteBatch sb) {
+        batch.setProjectionMatrix(camera.combined);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        bgSprite.draw(batch);
+        batch.end();}
 
     @Override
     public void dispose() {}
